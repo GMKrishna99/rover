ccecbb4ecc5edf568ddf66c16290de91f14d1ead
GMKrishna99 <gmohankrishna23@gmail.com>
2025-01-08 16:12:31 +0530

    removed auth

--- src/App.js
+++ src/App.js
@@ -7,14 +7,11 @@ import {
   useLocation,
 } from "react-router-dom";
 
-// import { CSSTransition } from "react-transition-group";
 import { ToastContainer } from "react-toastify"; // Import ToastContainer
 import "react-toastify/dist/ReactToastify.css"; // Import CSS for ToastContainer
-// import Navbar from "./components/Navbar";
 import Navbar from "./components/Navbar/navbar";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer";
-// import LoadingSpinner from "./components/Loader/Loader";
 import ProductDetails from "./components/Products/ProductDetails";
 import AddToCartPage from "./components/addtocart"; // Import the AddToCartPage component
 import CheckoutPage from "./components/Order/checkoutpage";
@@ -24,7 +21,6 @@ import OrderHistory from "./Component/OrderHistory";
 import Register from "./Component/Register";
 import NavbarCategory from "./components/Navbar/navbarcategories";
 import Profile from "./components/Order/profile";
-import PrivateRoute from "./route";
 
 const ScrollToTop = () => {
   const location = useLocation();
@@ -37,16 +33,7 @@ const ScrollToTop = () => {
 };
 
 function App() {
-  // const [loading, setLoading] = useState(true);
   const [cartItems, setCartItems] = useState([]);
-
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setLoading(false);
-  //   }, 2000); // Simulating loading for 1 second. Adjust as needed.
-
-  //   return () => clearTimeout(timer);
-  // }, []);
 
   const handleAddToCart = (product) => {
     let cart = [...cartItems, product];
@@ -62,24 +49,19 @@ function App() {
   };
 
   const handleMoveToWishlist = (index) => {
-    // Logic to move item to wishlist
     let cart = [...cartItems];
     cart.splice(index, 1);
     setCartItems(cart);
     localStorage.setItem("cart", JSON.stringify(cart));
     console.log("Moved to wishlist");
   };
-  const jwtToken = localStorage.getItem("jwtToken"); // Retrieve token from localStorage
 
   return (
     <div>
       <ToastContainer position="bottom-right" />
       <Router>
         <ScrollToTop />
-        {/* {!loading && <Navbar cartItems={cartItems}/>} */}
-        <Navbar cartItems={cartItems} />{" "}
-        {/* Render navbar only if not loading */}
-        {/* <Navbar cartItems={cartItems}/> */}
+        <Navbar cartItems={cartItems} />
         <Routes>
           <Route
             exact
@@ -97,64 +79,29 @@ function App() {
 
           <Route
             path="/product/:productId"
-            element={
-              <PrivateRoute
-                element={(props) => (
-                  <ProductDetails {...props} onAddToCart={handleAddToCart} />
-                )}
-                jwtToken={jwtToken}
-              />
-            }
+            element={<ProductDetails onAddToCart={handleAddToCart} />}
           />
           <Route
             path="/cart"
             element={
-              <PrivateRoute
-                element={(props) => (
-                  <AddToCartPage
-                    {...props}
-                    cartItems={cartItems}
-                    onAddToCart={handleAddToCart}
-                    onRemove={handleRemoveFromCart}
-                    onMoveToWishlist={handleMoveToWishlist}
-                  />
-                )}
-                jwtToken={jwtToken}
+              <AddToCartPage
+                cartItems={cartItems}
+                onAddToCart={handleAddToCart}
+                onRemove={handleRemoveFromCart}
+                onMoveToWishlist={handleMoveToWishlist}
               />
             }
           />
-          <Route
-            path="/checkout"
-            element={
-              <PrivateRoute element={CheckoutPage} jwtToken={jwtToken} />
-            }
-          />
-          <Route
-            path="/checkout/buynow"
-            element={
-              <PrivateRoute element={CheckoutPage} jwtToken={jwtToken} />
-            }
-          />
-          <Route
-            path="/product-list"
-            element={<PrivateRoute element={ProductList} jwtToken={jwtToken} />}
-          />
-          <Route
-            path="/profile/orderHistory"
-            element={
-              <PrivateRoute element={OrderHistory} jwtToken={jwtToken} />
-            }
-          />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/checkout/buynow" element={<CheckoutPage />} />
+          <Route path="/product-list" element={<ProductList />} />
+          <Route path="/profile/orderHistory" element={<OrderHistory />} />
           <Route
             path="/category/:gender/:type/:subtype"
-            element={
-              <PrivateRoute element={NavbarCategory} jwtToken={jwtToken} />
-            }
+            element={<NavbarCategory />}
           />
         </Routes>
-        {<Footer />}
-        {/* {!loading && <Footer />} */}
-        {/* {loading && <LoadingSpinner />}  */}
+        <Footer />
       </Router>
     </div>
   );
